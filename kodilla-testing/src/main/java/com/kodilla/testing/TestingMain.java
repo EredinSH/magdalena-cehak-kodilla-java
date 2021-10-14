a9d1c042657b995d63ad12f664819831e4476fc0
Magdalena Cehak-Milkowska <cehakm@gmail.com>
2021-10-14 13:11:40 +0200

    6.2 Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,20 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int result;
+
+        result = calculator.add(40,25);
+
+        System.out.println(result);
+
+        result = calculator.substract(60,37);
+
+        System.out.println(result);
     }
 }
 

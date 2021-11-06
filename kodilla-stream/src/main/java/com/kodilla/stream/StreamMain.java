ededa97f7197537e74b65b40e621e388184273bf
Magdalena Cehak-Milkowska <cehakm@gmail.com>
2021-11-06 19:25:32 +0100

    7.2 Immutable forum user.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,13 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
-
-import static java.awt.SystemColor.text;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        poemBeautifier.beautify("Litwo, ojczyzno moja!", (text) -> text + " Ty jestes jak zdrowie.");
-        poemBeautifier.beautify("Litwo, ojczyzno moja!", (text) -> text + " Adam Mickiewicz");
-        poemBeautifier.beautify("Litwo, ojczyzno moja!", (text) -> "~~<3 " + text + " <3~~");
-        poemBeautifier.beautify("Litwo, ojczyzno moja!", (text) -> text.toUpperCase());
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 

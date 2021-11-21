b8288195d3ed3822d3c4a115ab76efea41d071e9
Magdalena Cehak-Milkowska <cehakm@gmail.com>
2021-11-21 16:56:27 +0100

    8.4 Error handling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsMap.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsMap.java
@@ -1,19 +1,24 @@
 package com.kodilla.exception.test;
 
+import com.sun.jdi.Value;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class FlightsMap {
 
-    public void findFilght(Flight flight) throws Exception {
+    public boolean findFilght(Flight flight) throws Exception {
         HashMap<String, Boolean> flightsDetector = new HashMap<>();
         flightsDetector.put("Chopin",true);
         flightsDetector.put("Robin Hood", false);
         flightsDetector.put("Mikołaj Kopernik", true);
 
+        boolean flightScan = false;
+
         try {
             if (flightsDetector.containsKey(flight.getDepartureAirport()) == true && flightsDetector.containsKey(flight.getArrivalAirport()) == true) {
                 System.out.println("Twój lot z " + flight.getArrivalAirport() + " do " + flight.getDepartureAirport() + " jest w systemie.");
+                flightScan = true;
             } else {
                 throw new RouteNotFoundException("Błędnie wpisana nazwa lotniska");
             }
@@ -23,6 +28,8 @@ public class FlightsMap {
         } finally {
             System.out.println("Życzymy udanej podróży!");
         }
+
+        return flightScan;
     }
 
     public static void main(String[] args)throws Exception {

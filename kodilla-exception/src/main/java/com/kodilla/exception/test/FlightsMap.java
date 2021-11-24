bb7b21247a8f0f430f3430c69f0fb757335b3176
Magdalena Cehak-Milkowska <cehakm@gmail.com>
2021-11-24 16:26:12 +0100

    8.4 Error handling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsMap.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightsMap.java
@@ -13,29 +13,24 @@ public class FlightsMap {
         flightsDetector.put("Robin Hood", false);
         flightsDetector.put("Mikołaj Kopernik", true);
 
-        boolean flightScan = false;
-
-        try {
             if (flightsDetector.containsKey(flight.getDepartureAirport()) == true && flightsDetector.containsKey(flight.getArrivalAirport()) == true) {
-                System.out.println("Twój lot z " + flight.getArrivalAirport() + " do " + flight.getDepartureAirport() + " jest w systemie.");
-                flightScan = true;
+                return flightsDetector.get(flight.getDepartureAirport()) && flightsDetector.get(flight.getArrivalAirport());
             } else {
                 throw new RouteNotFoundException("Błędnie wpisana nazwa lotniska");
             }
-
-        } catch (RouteNotFoundException e) {
-            System.out.println("Wyszukiwanego lotniska nie ma w naszej ofercie.");
-        } finally {
-            System.out.println("Życzymy udanej podróży!");
-        }
-
-        return flightScan;
     }
 
     public static void main(String[] args)throws Exception {
         FlightsMap flightMap = new FlightsMap();
         Flight flight1 = new Flight("Chopin", "Las Vegas");
-        flightMap.findFilght(flight1);
+
+        try {
+            flightMap.findFilght(flight1);
+        } catch (RouteNotFoundException e) {
+            System.out.println("Wyszukiwanego lotniska nie ma w naszej ofercie.");
+        } finally {
+            System.out.println("Życzymy udanej podróży!");
+        }
     }
 
 }

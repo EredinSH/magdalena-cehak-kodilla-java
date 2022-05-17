cf59def789245caece5d2df67a319752e06d15dc
Magdalena Cehak-Milkowska <cehakm@gmail.com>
2022-05-17 17:16:35 +0200

    31.2 Facebook

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,37 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
+    public static final String MONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
+    public static final String YEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
+    public static final String COOKIES = "//div[contains(@class, \"_9xo5\")]/button[2]";
+    public static final String NEW_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a[1]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.facebook.com/");
+
+        driver.findElement(By.xpath(COOKIES)).click();
+        driver.findElement(By.xpath(NEW_ACCOUNT)).click();
+
+        WebElement selectDay = driver.findElement(By.xpath(DAY));
+        Select choosenDay = new Select(selectDay);
+        choosenDay.selectByIndex(12);
+
+        WebElement selectMonth = driver.findElement(By.xpath(MONTH));
+        Select choosenMonth = new Select(selectMonth);
+        choosenMonth.selectByIndex(6);
+
+        WebElement selectYear = driver.findElement(By.xpath(YEAR));
+        Select choosenYear = new Select(selectYear);
+        choosenYear.selectByIndex(28);
+    }
+
+}
